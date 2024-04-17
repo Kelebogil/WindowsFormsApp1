58c1d07e4346d068de5479f91dfd211cd5f12403
Kelebogil <kele.ply@gmail.com>
2024-04-17 09:55:54 +0200

    addning Stored procedures

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -57,6 +57,49 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
+            string gender = "";
+            if (radMale.Checked)
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "Female";
+            }
+
+            List<string> selectedLang = new List<string>();
+            if (checkSetswana.Chekeced)
+            {
+                selectedLang.Add("Setswana");
+            }
+            if (checkEnglish.Checked)
+            {
+                selectedLang.Add("English");
+            }
+            if (checkIsizulu.Checked)
+            {
+                selectedLang.Add("Isizulu");
+            }
+            if (checkAfrikaans.Checked)
+            {
+                selectedLang.Add("Afrikaans");
+            }
+
+            string languages = String.Join(",", selectedLang);
+
+            con.Open();
+            using (SqlCommand cmd= new SqlCommand("dbo.SP_Student_Insert", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Full_Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Index_no", txtIndexNumber.Text);
+                cmd.Parameters.AddWithValue("@Aca_year", comAcademicYear.Text);
+                cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@DOB", calDOB.Value);
+                cmd.Parameters.AddWithValue("@Language", languages);
+                cmd.ExecuteNonQuery();
+            }
 
         }
 
